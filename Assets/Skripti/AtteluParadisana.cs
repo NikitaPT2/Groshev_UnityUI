544c7932d24ef74c659fc1391c97713214221550
Nikita <nikitogka@gmail.com>
2021-03-18 19:49:59 +0200

    Izpildiju 4, 5 un 6 punktu

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour
+{
+    public GameObject pets;
+    public GameObject skin;
+    public GameObject head;
+
+    public void petsAttelosana(bool vertiba)
+    {
+        pets.SetActive(vertiba);
+    }
+
+    public void skinAttelosana(bool vertiba)
+    {
+        skin.SetActive(vertiba);
+    }
+
+    public void headAttelosana(bool vertiba)
+    {
+        head.SetActive(vertiba);
+    }
+}
---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VardaParadisana : MonoBehaviour{
+    public string teksts, vecums;
+    public GameObject ievadesLauks;
+    public GameObject ievadesLauks2;
+    public GameObject tekstaAttelosana;
+
+    public void uzglabatTekstu()
+    {
+
+        teksts = ievadesLauks.GetComponent<Text>().text;
+        vecums = ievadesLauks2.GetComponent<Text>().text;
+        tekstaAttelosana.GetComponent<Text>().text = "Supervaronis " + teksts + " ir " + vecums + " gadus vecs!";
+
+    }
+
+
+
+
+}
